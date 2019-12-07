2ae427dd07551d224b51c14984fc97595e32c62f
Lauren McCoy <lm_lutec@outlook.com>
2019-12-07 13:13:05 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'welcome#home'
+
+  get '/home' => 'welcome#home'
+
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+
 end
