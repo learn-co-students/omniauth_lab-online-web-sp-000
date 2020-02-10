eba3d03d0317a39be8f4d56982e7a97e67cc4f60
Educhin <c.s.arinze@gmail.com>
2020-02-10 03:12:44 -0500

    All test are passing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # Add your routes here
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  get '/', to: 'welcome#home'
+  get '/logout', to: 'sessions#destroy'
 end
