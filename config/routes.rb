fbe6ad9779274edc4f8716c1a666e1616cec4a52
jkellyphilly <joel.kelly.philly@gmail.com>
2020-05-20 15:18:47 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # Add your routes here
   root 'welcome#home'
 
-  match '/app/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
 end
