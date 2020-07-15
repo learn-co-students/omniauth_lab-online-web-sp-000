43431646af594676050c338652dfe3f6267cc7f1
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-07-15 18:23:13 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  #rack middleware
+  # use Rack::Session::Cookie
+  # use OmniAuth::Strategies::Developer
+  #
+  # config.session_store :cookie_store, key: '_interslice_session'
+  # config.middleware.use ActionDispatch::Cookies # Required for all session management
+  # config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # Add your routes here
+  root 'welcome#home'
+  #this route allows us to redirect users to '/auth/:provider'
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
 end
