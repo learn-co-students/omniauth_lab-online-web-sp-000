3ebb17e02c76c9d8ad4431484f218b7f9814f268
travelingwheels <gkwheeler40@gmail.com>
2020-07-15 21:28:51 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # Add your routes here
+  root 'welcome#home'
+  match '/app/github/callback', to: 'sessions#create', via: [:get, :post]
+  #get 	'/auth/github/callback', to:  'sessions#create'
+
+
 end
