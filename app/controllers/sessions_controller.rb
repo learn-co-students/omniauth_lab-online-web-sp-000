d2f3c0ffae649cfb54969ce23a21b8c9d91a2f1a
gabejoseph <gabriel.joseph4@gmail.com>
2020-07-21 15:37:51 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: :create
 
   def create
+    binding.pry
     # After entering a name and email value in the /auth/developer
     # path and submitting the form, you will see a pretty-print of
     # the authentication data object that comes from the "developer"
@@ -20,4 +21,5 @@ class SessionsController < ApplicationController
     # Ye olde redirect
     redirect_to root_path
   end
+
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :developer unless Rails.env.production?
-    provider :github, ENV['d13aa03885f77d91621e'], ENV['7f5166bb9ae6979aa8888ee5b06d2536cb9b4a83']
+    provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
 end
