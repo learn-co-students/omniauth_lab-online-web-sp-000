c7cb2086b3f5fc1b9f3c37f2d44b90a41f835e42
marcon29 <online@markstabler.com>
2020-10-01 19:23:43 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # Add your routes here
+
+  root "welcome#home"
+  delete "/logout", to: "sessions#destroy"
+
+  # from code along - looks like way to create multiple routes from same URL
+  match "/auth/developer/callback", to: "sessions#create", via: [:get, :post]
+  
+  get "/auth/github/callback", to: "sessions#create"
+  
+  
+  # normal route for OmniAuth
+  # get "/auth/developer/callback", to: "sessions#create"
+
+
 end
