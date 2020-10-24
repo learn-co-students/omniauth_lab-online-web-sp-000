e648eae41e17c00f6e0304258774b6d97638011a
Taishawnk <Taishawnking1@yahoo.com>
2020-10-23 22:03:29 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # Add your routes here
+  root 'welcome#home'
+  # we need to provide a route for the providers' strategies to "call back" to after the authenticating third-party decides whether the user has passed or failed authentication.
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
 end
