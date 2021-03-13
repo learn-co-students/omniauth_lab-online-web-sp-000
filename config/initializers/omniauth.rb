84c5cf0b0c09deeef96da8fedfce252c17a86c2e
RSigmaSG <sg.comparch@gmail.com>
2021-03-12 23:08:06 -0500

    Done.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+    OmniAuth.config.allowed_request_methods = [:post, :get]
     provider :developer unless Rails.env.production?
     provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
   end
