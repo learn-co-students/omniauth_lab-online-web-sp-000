66b99c7a6d2ff4e125257395a2031582279e46f7
Alan Tang <giabao_tang@yahoo.com>
2021-04-29 18:07:48 -0700

    Done.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :developer , {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}} unless Rails.env.production?
-     #provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+    #provider :developer , {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}} unless Rails.env.production?
+    provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
   end
