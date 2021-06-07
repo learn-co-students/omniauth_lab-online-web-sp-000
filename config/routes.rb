be9a99fc3a7a6e11ab045f3af34d0030296c851a
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-06-07 15:00:08 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root 'welcome#home'
   get '/home' => 'welcome#home'
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  # get '/auth/github/callback', to: 'sessions#create'
+  # post '/auth/github', to: 'welcome#home'
 end
